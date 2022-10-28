996089f6d753dad746103daf7173ab0d0853656f
Jahswill Ovedhe <jahswilling@gmail.com>
2022-10-28 08:53:35 +0100

    CORS fix

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://localhost:8080","*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
